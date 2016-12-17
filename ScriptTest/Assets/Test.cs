5d92fbbb4a5afc0e7231624bf657a846b9579e91
merchan kym <merchan5801@gmail.com>
2016-12-17 16:17:21 +0900

    no message

--- ScriptTest/Assets/Test.cs
+++ ScriptTest/Assets/Test.cs
@@ -9,13 +9,16 @@ public class Test : MonoBehaviour {
 		// 配列を初期化する
 		int[] points = {10, 20, 30, 40, 50,};
 
+		// 配列の要素数の正順で表示する
+		for (int i = 0; i < points.Length; i++)
+			   Debug.Log (points [i]);
+		
 		// 配列の要素数を逆順にする
 		for (int i = points.Length-1; 0 <= i; i--) {
-			
-
-				Debug.Log (points [i]);
+			   Debug.Log (points [i]);
 			
 		}
+
 	}
 
 	// Update is called once per frame
