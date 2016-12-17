4c7d374118bc0068cd17f4b436174d1de59a4c07
merchan kym <merchan5801@gmail.com>
2016-12-17 15:21:33 +0900

    Lesson4 課題：配列を宣言して出力しましょうのレビューをお願いします。

---
+++ ScriptTest/Assets/Test.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// 配列を初期化する
+		int[] points = {10, 20, 30, 40, 50,};
+
+		// 配列の要素数を逆順にする
+		for (int i = points.Length-1; 0 <= i; i--) {
+			
+
+				Debug.Log (points [i]);
+			
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
